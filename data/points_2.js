edd2927b7fa3ccb9b99387ff510e79f7fa526251
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-24 22:46:42 +0800

    Add files via upload

---
+++ data/points_2.js
@@ -0,0 +1,11 @@
+var json_points_2 = {
+"type": "FeatureCollection",
+"name": "points_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Rumah Milea", "desc": "Jl. Kencana No.3, Malabar, Kec. Lengkong, Kota Bandung, Jawa Barat 40262, Indonesia", "X": "107.621776", "Y": "-6.9250039" }, "geometry": { "type": "Point", "coordinates": [ 107.6217768, -6.9250039 ] } },
+{ "type": "Feature", "properties": { "name": "Warung Bi Eem", "desc": "Warung Bi Eem, Jl. Panaitan No.22, Kebon Pisang, Sumurbandung, Bandung City, West Java 40112, Indonesia", "X": "107.617184", "Y": "-6.9184101" }, "geometry": { "type": "Point", "coordinates": [ 107.6171843, -6.9184101 ] } },
+{ "type": "Feature", "properties": { "name": "Kopi Gandapoera", "desc": "Kopi Gandapoera, Jl. Gandapura No.14, Merdeka, Sumurbandung, Bandung City, West Java 40113, Indonesia", "X": "107.625898", "Y": "-6.9157543" }, "geometry": { "type": "Point", "coordinates": [ 107.6258989, -6.9157543 ] } },
+{ "type": "Feature", "properties": { "name": "GOR Saparua", "desc": "GOR dan Taman Saparua, Jl. Banda No.28, Citarum, Bandung Wetan, Bandung City, West Java 40115, Indonesia", "X": "107.616186", "Y": "-6.908557" }, "geometry": { "type": "Point", "coordinates": [ 107.6161867, -6.908557 ] } }
+]
+}
